65bc6b63c70b7a52fac6ca08b7ab22d9cbaa2be9
Aries Camitan <arc@sitecore.net>
2020-01-13 17:49:46 +0800

    Add Data. BookModel and BookService

---
+++ Data/Services/IBookService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Summaries.Data
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+        Book GetBookById(int Id);
+        void UpdateBook(int id, Book newBook);
+        void DeleteBook(int id);
+        void AddBook(Book newBook);
+    }
+}
