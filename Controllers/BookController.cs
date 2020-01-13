2f9c66290bb57bc1a1814ae46fce14ffa34e6ff8
Aries Camitan <arc@sitecore.net>
2020-01-13 19:53:12 +0800

    Add book service API

---
+++ Controllers/BookController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Summaries.Data;
+
+namespace Summaries.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookService  _service;
+
+        public BookController(IBookService  service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("AddBook")]
+        public IActionResult AddBook([FromBody]Book book)
+        {
+            _service.AddBook(book);
+            return Ok("Added");
+        }
+
+    }
+}
